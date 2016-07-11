6acc866605c59018b9b5bc6da5933170fc960895
Vadim <amdarisdev@gmail.com>
2016-07-11 17:57:59 +0300

    create menu

---
+++ FileSystemAndStreams/FileSystemAndStreams/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemAndStreams
+{
+    public class Menu
+    {
+        private string inputComand;
+        
+        public void MainMenu()
+        {
+
+            //switch (inputComand)
+            //{
+            //    case "q":
+
+            //    default:
+            //}
+        }
+    }
+}
--- FileSystemAndStreams/FileSystemAndStreams/Program.cs
+++ FileSystemAndStreams/FileSystemAndStreams/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,36 @@ namespace FileSystemAndStreams
 {
     class Program
     {
+        static void MainMenu()
+        {
+            Console.Write("Enter a number: ", System.Text.Encoding.UTF8);
+            string inputString = Console.ReadLine();
+            int intResult;
+            int.TryParse(inputString, out intResult);
+            Console.WriteLine("Entered number: {0}", intResult);
+            if (intResult == 0)
+            {
+                return;
+            }
+            MainMenu();
+        }
         static void Main(string[] args)
         {
+
+            MainMenu();
+            //DriveInfo[] driveInfo = DriveInfo.GetDrives();
+
+            //foreach (var item in driveInfo)
+            //{
+            //    Console.WriteLine($"Name: {item.Name}");
+            //    Console.WriteLine($"DriveType: {item.DriveType}");
+            //    if (item.IsReady)
+            //    {
+            //        Console.WriteLine($"Drives are ready: {item.IsReady}");
+            //        Console.WriteLine($"Total Free space: {item.TotalFreeSpace}");
+            //    }
+            //}
+            Console.Read();
         }
     }
 }
